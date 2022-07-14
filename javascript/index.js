6a09ea43b0ca0b2f6e443bb1e048ebd50269870e
Felicia Winna <winna.felicia@gmail.com>
2022-07-14 23:43:37 +0800

    Added owl carousel

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
-// research for "carousel"
+// carousel function
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
