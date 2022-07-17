a4537ddae9ec8a6ab4e68f5d970137bb6744d6c9
Felicia Winna <winna.felicia@gmail.com>
2022-07-17 19:27:53 +0800

    Fixed gallery scroll

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,3 @@
 function scrollDown(){
     window.scroll(0,findPos(document.getElementById("abtTitle")));
 }
-
-function findPos(obj) {
-    var curtop = 0;
-    if (obj.offsetParent) {
-        do {
-            curtop += obj.offsetTop;
-        } while (obj = obj.offsetParent);
-    return [curtop];
-    }
-}
