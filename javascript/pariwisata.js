1d66245dec720718eac8898648c793501d9533b1
Felicia Winna <winna.felicia@gmail.com>
2022-07-19 02:39:17 +0800

    Fixed autoscroll

--- javascript/pariwisata.js
+++ javascript/pariwisata.js
@@ -1,14 +1,17 @@
 // clickable image functions
 function goToSawit(){
     window.scroll(0,findPos(document.getElementById("sawit")));
+    console.log("sawit")
 }
 
 function goToPendem(){
     window.scroll(0,findPos(document.getElementById("pendem")));
+    console.log('pendem')
 }
 
 function goToGogik(){
     window.scroll(0,findPos(document.getElementById("gogik")));
+    console.log('gogik')
 }
 
 function findPos(obj) {
